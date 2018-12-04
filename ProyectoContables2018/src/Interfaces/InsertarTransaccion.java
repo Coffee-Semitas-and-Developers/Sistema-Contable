4a91e894db016bb8301c8b6e3979df00ccc00b79
Daniel Mejia <rdanielmejia7@gmail.com>
2018-12-04 09:26:22 -0600

    aaa

--- ProyectoContables2018/src/Interfaces/InsertarTransaccion.java
+++ ProyectoContables2018/src/Interfaces/InsertarTransaccion.java
@@ -181,7 +181,12 @@ public class InsertarTransaccion extends javax.swing.JFrame {
         jLabel12 = new javax.swing.JLabel();
         txtDebe = new javax.swing.JTextField();
         txtHaber = new javax.swing.JTextField();
-        btnGestionar = new javax.swing.JButton();
+        btnGuardar1 = new javax.swing.JButton();
+        txtDebe1 = new javax.swing.JTextField();
+        txtHaber1 = new javax.swing.JTextField();
+        jLabel13 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        lbPartidaDoble1 = new javax.swing.JLabel();
 
         jButton2.setText("jButton2");
 
@@ -242,7 +247,6 @@ public class InsertarTransaccion extends javax.swing.JFrame {
             }
         });
 
-
         jLabel11.setText("SELECCIONE CUENTAS A UTILIZAR");
 
         btnGCreditoDebito.add(rbIVACredito);
@@ -258,7 +262,6 @@ public class InsertarTransaccion extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-
                         .addGap(52, 52, 52)
                         .addComponent(jLabel11))
                     .addGroup(jPanel1Layout.createSequentialGroup()
@@ -270,10 +273,6 @@ public class InsertarTransaccion extends javax.swing.JFrame {
                             .addComponent(btnAceptar))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel6)
-                            .addComponent(jLabel4))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(rbIVACredito)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -285,7 +284,8 @@ public class InsertarTransaccion extends javax.swing.JFrame {
                                 .addComponent(rbDebe)
                                 .addGap(44, 44, 44)
                                 .addComponent(rbHaber)))))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)));
+                .addContainerGap(15, Short.MAX_VALUE))
+        );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
@@ -310,8 +310,9 @@ public class InsertarTransaccion extends javax.swing.JFrame {
                         .addComponent(rbIVACredito)
                         .addComponent(rbIVADebito)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-
         );
+
+        getContentPane().add(jPanel1);
 
         jLabel5.setText("Fecha");
 
@@ -365,7 +366,10 @@ public class InsertarTransaccion extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
+        getContentPane().add(jPanel5);
+
         jLabel10.setText("INSERTAR TRANSACCION");
+        getContentPane().add(jLabel10);
 
         jLabel2.setText("Detalles de transaccion");
 
@@ -376,6 +380,7 @@ public class InsertarTransaccion extends javax.swing.JFrame {
         lbPartidaDoble.setText("No cumple partida doble");
 
         jLabel12.setText("Total (Dolares)");
+
         txtDebe.setEditable(false);
 
         txtHaber.setEditable(false);
@@ -424,49 +429,30 @@ public class InsertarTransaccion extends javax.swing.JFrame {
                 .addGap(10, 10, 10))
         );
 
-        btnGestionar.setText("GESTIONAR TRANSACCION");
-        btnGestionar.addActionListener(new java.awt.event.ActionListener() {
+        getContentPane().add(jPanel2);
+
+        btnGuardar1.setText("Guardar");
+        btnGuardar1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnGestionarActionPerformed(evt);
+                btnGuardarActionPerformed(evt);
             }
         });
+        getContentPane().add(btnGuardar1);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(14, 14, 14)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(74, 74, 74)
-                        .addComponent(jLabel10)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(btnGestionar))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jPanel5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(18, 18, 18)
-                        .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                .addContainerGap())
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnGestionar)
-                    .addComponent(jLabel10))
-                .addGap(2, 2, 2)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(jPanel5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(24, Short.MAX_VALUE))
-        );
+        txtDebe1.setEditable(false);
+        getContentPane().add(txtDebe1);
+
+        txtHaber1.setEditable(false);
+        getContentPane().add(txtHaber1);
+
+        jLabel13.setText("Total (Dolares)");
+        getContentPane().add(jLabel13);
+
+        jLabel8.setText("Detalles de transaccion");
+        getContentPane().add(jLabel8);
+
+        lbPartidaDoble1.setText("GESTIONAR TRANSACCION");
+        getContentPane().add(lbPartidaDoble1);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -938,21 +924,24 @@ return cuenta;
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAceptar;
+    private javax.swing.ButtonGroup btnGCreditoDebito;
     private javax.swing.ButtonGroup btnGDebeHaber;
-    private javax.swing.JButton btnGestionar;
     private javax.swing.JButton btnGuardar;
+    private javax.swing.JButton btnGuardar1;
     private javax.swing.JComboBox<Cuenta> cmbCuenta;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
@@ -961,13 +950,16 @@ return cuenta;
     private javax.swing.JTable jTable1;
     private com.toedter.calendar.JDateChooser jdfecha;
     private javax.swing.JLabel lbPartidaDoble;
+    private javax.swing.JLabel lbPartidaDoble1;
     private javax.swing.JRadioButton rbDebe;
     private javax.swing.JRadioButton rbHaber;
     private javax.swing.JRadioButton rbIVACredito;
     private javax.swing.JRadioButton rbIVADebito;
     private javax.swing.JTextField txtDebe;
+    private javax.swing.JTextField txtDebe1;
     private javax.swing.JTextField txtDescripcion;
     private javax.swing.JTextField txtHaber;
+    private javax.swing.JTextField txtHaber1;
     private javax.swing.JTextField txtMonto;
     // End of variables declaration//GEN-END:variables
 
